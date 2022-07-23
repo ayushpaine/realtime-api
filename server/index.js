8aedb53bfea6ae13f84b170ba9597ef3ad29db69
ayushpaine <ayushmt701@gmail.com>
2022-07-23 23:15:16 +0530

    set, get, delete rules' functions added

---
+++ server/index.js
@@ -0,0 +1,61 @@
+const needle = require("needle");
+const config = require("dotenv").config();
+const TOKEN = process.env.TWITTER_BEARER_TOKEN;
+
+const rulesURL = "https://api.twitter.com/2/tweets/search/stream/rules";
+const streamURL =
+  "https://api.twitter.com/2/tweets/search/stream?tweet.field=public_metrics&expansions=author_id";
+
+const rules = [{ value: "giveaway" }];
+
+async function getRules() {
+  const response = await needle("get", rulesURL, {
+    headers: { Authorization: `Bearer ${TOKEN}` },
+  });
+
+  return response.body;
+}
+
+async function setRules() {
+  const data = {
+    add: rules,
+  };
+  const response = await needle("post", rulesURL, data, {
+    content_type: "application/json",
+    headers: { Authorization: `Bearer ${TOKEN}` },
+  });
+
+  return response.body;
+}
+
+async function deleteRules(rules) {
+  if (!Array.isArray(rules.data)) {
+    return null;
+  }
+
+  const ids = rules.data.map((rule) => rule.id);
+
+  const data = {
+    delete: {
+      ids: ids,
+    },
+  };
+  const response = await needle("post", rulesURL, data, {
+    content_type: "application/json",
+    headers: { Authorization: `Bearer ${TOKEN}` },
+  });
+
+  return response.body;
+}
+
+(async () => {
+  let currentRules;
+  try {
+    currentRules = await getRules();
+    await deleteRules(currentRules);
+    await setRules();
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+})();
